b414f227d7a8235a858fdc915ceb7564dc95a6b9
Artem <kovalenkoartem990@gmail.com>
2021-12-31 02:10:23 +0200

    Delete Property

---
+++ KnowledgeAccountingSystem/DAL/Migrations/20211231000420_DeleteKnowledgeLevelProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class DeleteKnowledgeLevelProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KnowledgeLevel",
+                table: "PersonSkills");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "KnowledgeLevel",
+                table: "PersonSkills",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
