278a0096a9cd3d5746e6dd87f129d4d8ade06a51
Artem <kovalenkoartem990@gmail.com>
2022-01-18 01:51:26 +0200

    Update Custom Exception class

--- KnowledgeAccountingSystem/BLL/Validation/KASException.cs
+++ KnowledgeAccountingSystem/BLL/Validation/KASException.cs
@@ -5,8 +5,14 @@ namespace BLL.Validation
     /// <summary>
     /// Represents error occuring during Knowledge Accounting System application execution.
     /// </summary>
+    [Serializable]
     public class KASException : Exception
     {
+        public KASException() { }
+
         public KASException(string massage) : base(massage) { }
+
+        public KASException(string message, Exception innerException)
+            : base(message, innerException) { }
     }
 }
